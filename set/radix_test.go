ab356d167d0a3a54f23a654ce83073bfd7d5d6ea
- <->
2019-07-14 10:25:17 +0800

    .

---
+++ set/radix_test.go
@@ -0,0 +1,24 @@
+package radix
+
+import (
+	"testing"
+)
+
+func TestRadix(t *testing.T) {
+	r:=New()
+	key := []byte("test")
+	r.Add(key,1)
+
+	val, ok := r.Get(key)
+	t.Logf("%s %d %t", key, val, ok)
+
+	key = []byte("test123")
+
+	key, val, ok = r.LongestPrefix(key)
+	t.Logf("%s %d %t", key, val, ok)
+
+	r.WalkPrefix([]byte("t"), func(s []byte, v interface{}) bool {
+		t.Logf("%s %d", s, v)
+		return true
+	})
+}
